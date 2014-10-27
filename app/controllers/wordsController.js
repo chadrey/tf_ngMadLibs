accca56842b56e0e452f20cedc47f6f68ebc63b3
Chad Reynoldson <chad@reynoldsoncontrol.com>
2014-10-27 11:12:53 -0500

    Added form data validation.

--- app/controllers/wordsController.js
+++ app/controllers/wordsController.js
@@ -7,14 +7,35 @@
   angular.module('app').controller('wordsController', wordsController);
   
   function wordsController($scope) {
-    $scope.male_name      = 'male name';
-    $scope.dirty_task     = 'Dirty task';
-    $scope.obn_celeb      = 'Obnoxious celebrity';
-    $scope.job_title      = 'Job title';
-    $scope.celeb          = 'Celebrity';
-    $scope.huge_number    = 'Huge number';
-    $scope.tedious_task   = 'Tedious task';
-    $scope.useless_skill  = 'Useless skill';
-    $scope.adjective      = 'Adjective';
+
+    $scope.page = "input";
+
+    
+    /* Submit user input.
+    ------------------------------*/
+    $scope.submit = function() {
+      if($scope.myForm.$valid) {
+        $scope.page = "output";
+      }
+    }
+
+    /* Reset user input.
+    ------------------------------*/
+    $scope.reset = function() {
+        $scope.male_name      = "";
+        $scope.dirty_task     = "";
+        $scope.obn_celeb      = "";
+        $scope.job_title      = "";
+        $scope.celeb          = "";
+        $scope.huge_number    = "";
+        $scope.tedious_task   = "";
+        $scope.useless_skill  = "";
+        $scope.adjective      = "";
+      
+        $scope.page = "input";
+        $scope.submitted = false;
+    }
   }
+
+  
 }());
